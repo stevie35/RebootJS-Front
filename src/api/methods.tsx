c5f4e0094d54369603cc40e49c820229d26db030
stevie35 <stevie.leveque@outlook.com>
2020-09-10 14:28:26 +0200

    Add style for LoginForm

--- src/api/methods.tsx
+++ src/api/methods.tsx
@@ -1,11 +1,23 @@
 import { User } from "../users/types";
 import axios from 'axios';
+import { IProfile } from "../profile/types";
 
 // fetch users via the server
 export function getUsers(): Promise<User[]> {
-  return axios.get('http://localhost:3000/profiles')
+  return axios.get('http://localhost:3000/profiles') // return axios.get('http://localhost:3000/profile')
     .then(resp => {
       return resp.data
     })
 }
+
+export function login(email: string, password: string): Promise<IProfile>{
+    return axios
+      .post(
+        'http://localhost:3000/login',
+        {
+          username: email,
+          password: password
+        })
+      .then(resp => resp.data) 
+    }
 
