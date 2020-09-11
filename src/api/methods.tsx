1634153bae6b05e2b9c4d8f15134475f912d3126
stevie35 <stevie.leveque@outlook.com>
2020-09-11 12:26:17 +0200

    Fix route problems : profile sans s

--- src/api/methods.tsx
+++ src/api/methods.tsx
@@ -4,7 +4,9 @@ import { IProfile } from "../profile/types";
 
 // fetch users via the server
 export function getUsers(): Promise<User[]> {
-  return axios.get('http://localhost:3000/profiles', // return axios.get('http://localhost:3000/profile')
+  //return axios.get('http://localhost:3000/profiles',
+  // return axios.get('http://localhost:3000/profile')
+  return axios.get(`${process.env.REACT_APP_BACKEND}/profile`,
   {
     withCredentials: true
   })
@@ -16,7 +18,8 @@ export function getUsers(): Promise<User[]> {
 export function login(email: string, password: string): Promise<IProfile>{
     return axios
       .post(
-        'http://localhost:3000/login',
+        // 'http://localhost:3000/login',
+        `${process.env.REACT_APP_BACKEND}/login`,
         {
           username: email,
           password: password
@@ -26,5 +29,11 @@ export function login(email: string, password: string): Promise<IProfile>{
         }
         )
       .then(resp => resp.data) 
+    }
+
+    export function register(email: string, password: string, firstname: string, lastname: string) : Promise<IProfile>{
+        return axios.post(`${process.env.REACT_APP_BACKEND}/profile`, { email, password, firstname, lastname })
+          .then(resp => resp.data);
+
     }
 
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -2,7 +2,34 @@ export interface IFormField{
     value: string;
     isValid: boolean;
   }
+
+  export interface IProfileFormFields {
+    email: IFormField;
+    password: IFormPasswordField;
+    confirmation: IFormField;
+    firstname: IFormField;
+    lastname: IFormField;
+  }
+  
+  export interface IFormPasswordField extends IFormField {
+    hasLower: boolean;
+    hasUpper: boolean;
+    hasNumber: boolean;
+    hasSymbol: boolean;
+    hasValidLength: boolean;
+  }
   
   export function defaultFormField(): IFormField {
     return { value: '', isValid: true };
   }
+
+  export function defaultPasswordField(): IFormPasswordField {
+    return {
+      ...defaultFormField(),
+      hasLower: false,
+      hasUpper: false,
+      hasNumber: false,
+      hasSymbol: false,
+      hasValidLength: false
+    }
+}
